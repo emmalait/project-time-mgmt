92cfec25573025a8852b41410aee5fd63de6e478
Emma Laitinen <emlaitine@gmail.com>
2019-02-22 00:32:19 +0200

    Added role functionality

--- application/__init__.py
+++ application/__init__.py
@@ -15,6 +15,52 @@ else:
 
 db = SQLAlchemy(app)
 
+# Login functionality
+from application.auth.models import User
+from os import urandom
+app.config["SECRET_KEY"] = urandom(32)
+
+from flask_login import LoginManager, current_user
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+login_manager.login_view = "auth_login"
+login_manager.login_message = "Please log in to use this functionality."
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+# Roles in login_required
+from functools import wraps
+
+def login_required(role="ANY"):
+    def wrapper(fn):
+        @wraps(fn)
+        def decorated_view(*args, **kwargs):
+            if not current_user:
+                return login_manager.unauthorized()
+
+            if not current_user.is_authenticated():
+                return login_manager.unauthorized()
+            
+            unauthorized = False
+
+            if role != "ANY":
+                unauthorized = True
+                
+                for user_role in current_user.roles():
+                    if user_role == role:
+                        unauthorized = False
+                        break
+
+            if unauthorized:
+                return login_manager.unauthorized()
+            
+            return fn(*args, **kwargs)
+        return decorated_view
+    return wrapper
+
 # App functionalities
 from application import views
 
@@ -32,22 +78,6 @@ from application.customers import views
 
 from application.projects import models
 from application.projects import views
-
-# Login
-from application.auth.models import User
-from os import urandom
-app.config["SECRET_KEY"] = urandom(32)
-
-from flask_login import LoginManager
-login_manager = LoginManager()
-login_manager.init_app(app)
-
-login_manager.login_view = "auth_login"
-login_manager.login_message = "Please log in to use this functionality."
-
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(user_id)
 
 # Create db tables if necessary
 try:
