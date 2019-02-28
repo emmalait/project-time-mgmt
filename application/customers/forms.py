fb54187222216ba8444c0198ce92d876f1e807bb
Emma Laitinen <emlaitine@gmail.com>
2019-02-28 15:42:22 +0200

    Edit functionality to customer

--- application/customers/forms.py
+++ application/customers/forms.py
@@ -1,7 +1,15 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, validators
+from wtforms import StringField, HiddenField, validators
 
 class CustomerForm(FlaskForm):
+    name = StringField("Name", [validators.Length(min=2, max=144)])
+    business_id = StringField("Business ID", [validators.Length(min=9, max=9)])
+
+    class Meta:
+        csrf = False
+
+class CustomerEditForm(FlaskForm):
+    id = HiddenField()
     name = StringField("Name", [validators.Length(min=2, max=144)])
     business_id = StringField("Business ID", [validators.Length(min=9, max=9)])
 
--- application/customers/views.py
+++ application/customers/views.py
@@ -3,14 +3,15 @@ from flask_login import login_required
 
 from application import app, db
 from application.customers.models import Customer
-from application.customers.forms import CustomerForm
+from application.customers.forms import CustomerForm, CustomerEditForm
 
 # Show all customers
 @app.route("/customers", methods=["GET"])
+@login_required
 def customers_index():
     return render_template("customers/list.html", customers = Customer.query.all())
 
-# Create a new work type
+# Create a new customer
 @app.route("/customers", methods=["POST"])
 @login_required
 def customers_create():
@@ -26,16 +27,31 @@ def customers_create():
   
     return redirect(url_for("customers_index"))
     
-# Show the form for creating a new work type
+# Show the form for creating a new customer
 @app.route("/customers/new")
 @login_required
 def customers_form():
     return render_template("customers/new.html", form = CustomerForm())
 
-# View a work type
-@app.route("/customers/<customer_id>", methods=["GET"])
+# GET = show the edit form for a customer, POST = edit a work type
+@app.route("/customers/<customer_id>", methods=["GET", "POST"])
 @login_required
 def customer(customer_id):
     c = Customer.query.filter_by(id = customer_id).first()
+
+    if request.method == "GET":
+        return render_template("customers/view.html", form = CustomerEditForm(obj = c))
+
+    form = CustomerEditForm(request.form)
+
+    c = Customer.query.filter_by(id = form.id.data).first()
+
+    if not form.validate():
+        return render_template("customers/view.html", form = form)
+
+    c.name = form.name.data
+    c.business_id = form.business_id.data
+
+    db.session().commit()
   
-    return render_template("customers/view.html", customer = c)
+    return redirect(url_for("customers_index"))
