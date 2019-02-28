1a387aca4abdac7dde95675c9b7968d97fdbd604
Emma Laitinen <emlaitine@gmail.com>
2019-02-28 21:47:54 +0200

    ProjectMembers association table added & edited queries to use params for input

--- application/projects/models.py
+++ application/projects/models.py
@@ -7,12 +7,18 @@ from sqlalchemy import text
 
 import os
 
+ProjectMembers = db.Table('project_members',
+    db.Column('account_id', db.Integer, db.ForeignKey('account.id')),
+    db.Column('project_id', db.Integer, db.ForeignKey('project.id'))
+)
+
 class Project(Base):
 
     name = db.Column(db.String(144), nullable=False)
     budget = db.Column(db.Numeric(scale=2), nullable=False)
-
     customer_id = db.Column(db.Integer, db.ForeignKey('customer.id'), nullable=False)
+
+    members = db.relationship('User', secondary=ProjectMembers, backref='projects', lazy=True)
 
     def __init__(self, name, budget):
         self.name = name
@@ -20,16 +26,17 @@ class Project(Base):
     
     @staticmethod
     def get_costs(project_id):
+        # Use a different version of the query for Heroku because of PostgreSQL's boolean handling
         if os.environ.get("HEROKU"):
             stmt = text("SELECT SUM(time_log.hours * account.salary) FROM project"
                             " LEFT JOIN time_log ON project.id = time_log.project_id"
                             " LEFT JOIN account ON time_log.account_id = account.id"
-                            " WHERE project.id = " + str(project_id) + " AND time_log.cleared = 't'")
+                            " WHERE project.id = :project_id AND time_log.cleared = 't'").params(project_id = project_id)
         else:
             stmt = text("SELECT SUM(time_log.hours * account.salary) FROM project"
                             " LEFT JOIN time_log ON project.id = time_log.project_id"
                             " LEFT JOIN account ON time_log.account_id = account.id"
-                            " WHERE project.id = " + str(project_id) + " AND time_log.cleared = 1")
+                            " WHERE project.id = :project_id AND time_log.cleared = 1").params(project_id = project_id)
 
         res = db.engine.execute(stmt)
         
@@ -41,20 +48,26 @@ class Project(Base):
 
         costs = response[0].get("costs")
 
+        if costs:
+            costs = float(str(costs))
+        else:
+            costs = float(str(0))
+
         return costs
     
     @staticmethod
     def get_revenues(project_id):
+        # Use a different version of the query for Heroku because of PostgreSQL's boolean handling
         if os.environ.get("HEROKU"):
             stmt = text("SELECT SUM(time_log.hours * work_type.price) FROM project"
                             " LEFT JOIN time_log ON project.id = time_log.project_id"
                             " LEFT JOIN work_type ON time_log.work_type_id = work_type.id"
-                            " WHERE project.id = " + str(project_id) + " AND time_log.cleared = 't'")
+                            " WHERE project.id = :project_id) AND time_log.cleared = 't'").params(project_id = project_id)
         else:
             stmt = text("SELECT SUM(time_log.hours * work_type.price) FROM project"
                         " LEFT JOIN time_log ON project.id = time_log.project_id"
                         " LEFT JOIN work_type ON time_log.work_type_id = work_type.id"
-                        " WHERE project.id = " + str(project_id) + " AND time_log.cleared = 1")
+                        " WHERE project.id = :project_id AND time_log.cleared = 1").params(project_id = project_id)
 
         res = db.engine.execute(stmt)
         
@@ -65,5 +78,10 @@ class Project(Base):
             })
 
         revenues = response[0].get("revenues")
+
+        if revenues:
+            revenues = float(str(revenues))
+        else:
+            revenues = float(str(0))
 
         return revenues
--- application/projects/views.py
+++ application/projects/views.py
@@ -2,6 +2,8 @@ from flask import redirect, render_template, request, url_for
 from flask_login import login_required, current_user
 
 from application import app, db
+
+from application.auth.models import User
 from application.projects.models import Project
 from application.projects.forms import ProjectForm
 from application.customers.models import Customer
@@ -15,6 +17,9 @@ def projects_index():
 
     for project in ps:
         project.customer_name = Customer.query.filter_by(id = project.customer_id).first().name
+        project.budget = float(str(project.budget))
+        project.costs = Project.get_costs(project.id)
+        project.revenues = Project.get_revenues(project.id)
 
     return render_template("projects/list.html", projects = ps)
 
@@ -50,19 +55,9 @@ def project(project_id):
     p.customer_name = Customer.query.filter_by(id = p.customer_id).first().name
 
     p.budget = float(str(p.budget))
-    costs = Project.get_costs(project_id)
-
-    if costs:
-        p.costs = float(str(Project.get_costs(project_id)))
-    else:
-        p.costs = float(str(0))
-
-    revenues = Project.get_revenues(project_id)
-
-    if revenues:
-        p.revenues = float(str(Project.get_revenues(project_id)))
-    else:
-        p.revenues = float(str(0))
+    p.costs = Project.get_costs(project_id)
+    p.revenues = Project.get_revenues(project_id)
+    p.users = p.members
 
     cleared_tls = TimeLog.find_cleared_timelogs_by_project(project_id)
     uncleared_tls = TimeLog.find_uncleared_timelogs_by_project(project_id)
@@ -72,3 +67,36 @@ def project(project_id):
         project = p,
         cleared_timelogs = cleared_tls,
         uncleared_timelogs = uncleared_tls)
+
+# View page to assign users to a project
+@app.route("/projects/<project_id>/members", methods=["GET"])
+@login_required
+def assignedUsers(project_id):
+    p = Project.query.filter_by(id = project_id).first()
+    us = User.query.all()
+    
+    return render_template("projects/assignedUsers.html", project = p, users = us)
+
+
+# Assign user to a project
+@app.route("/projects/<project_id>/members/<account_id>/assign", methods=["POST"])
+@login_required
+def assignUser(project_id, account_id):
+    p = Project.query.filter_by(id = project_id).first()
+    p.members.append(User.query.get(account_id))
+
+    db.session().commit()
+
+    return redirect(request.referrer)
+
+# Unassign user to a project
+@app.route("/projects/<project_id>/members/<account_id>/unassign", methods=["POST"])
+@login_required
+def unassignUser(project_id, account_id):
+
+    p = Project.query.filter_by(id = project_id).first()
+    p.members.remove(User.query.get(account_id))
+
+    db.session().commit()
+
+    return redirect(request.referrer)
